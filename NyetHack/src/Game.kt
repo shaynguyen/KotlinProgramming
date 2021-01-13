647157c76a879ddab718c09b7ca9fbcb61c5443c
shay nguyen <shay.nguyen@gmail.com>
2021-01-13 00:46:03 -0800

    Ch04 - Functions

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -3,9 +3,28 @@ fun main() {
     var healthPoints = 89
     var isBlessed = true
     var isImmortal = false
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    val healthStatus = formatHealthStatus(healthPoints, name, isBlessed)
+
+    // player status
+    printPlayerStatus(name, healthStatus, auraColor)
+
+    castFireBall()
+}
+
+private fun printPlayerStatus(name: String, healthStatus: String, auraColor: String) {
+    println("$name $healthStatus")
+    println(auraColor)
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
 
+private fun formatHealthStatus(healthPoints: Int, name: String, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         100 -> "$name is in excellent condition!"
         in 90..99 -> "$name has a few scratches."
@@ -17,10 +36,12 @@ fun main() {
         in 15..74 -> "$name looks pretty hurt."
         else -> "$name is in awful condition!"
     }
-
-    // player status
-    println("$name $healthStatus")
-
-    // aura
-    println(auraColor)
+    return healthStatus
 }
+
+private fun castFireBall(numFireballs: Int = 2) {
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
+}
+
+private fun addOne(value: Int): Int =
+        value + 1
